334e205d334c854d005cc2f78393d154f7a7b050
Patrick Nüser <patrick.schnetger@gmail.com>
2024-06-18 20:40:48 +0200

    add anonymous profiles

--- components/ParticipantsList.vue
+++ components/ParticipantsList.vue
@@ -32,7 +32,7 @@ defineProps<{
             <td
               class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0"
             >
-              {{ participation?.profiles?.username }}
+              {{ participation?.profiles?.username || "Anonym" }}
             </td>
             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
               {{ participation.target_time }}
